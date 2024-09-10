931c67fa0ae42ea5b4908e768ad8ef734a7ac011
lucascordeiro <lucascordeirosimao@gmail.com>
2024-09-09 22:51:55 -0300

    1. twoSum

---
+++ twoSum.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <unordered_map>
+
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> num_map;
+        
+        for( int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if(num_map.find(complement) != num_map.end()){
+                return {i, num_map[complement]};
+            }
+
+            num_map[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
